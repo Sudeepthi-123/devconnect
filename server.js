1012415bfb307b45513350015838de4335bf35c2
Sudeepthi <sudeepthimaddi89@gmail.com>
2022-08-24 21:14:25 +0530

    Successfully made a connection to database

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express =require('express');
+const connectDB=require('./config/db');
 const app=express();
+
+// Connect Database
+
+connectDB();
+
+
 app.get('/',(req,res)=>res.send('API Running........11'))
 const PORT =process.env.PORT ||5000;
 app.listen(PORT,()=>console.log(`Server Started on port ${PORT}`));
