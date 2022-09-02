05ba392d6f992b723782082856ddee9a609b0ef8
Sudeepthi <sudeepthimaddi89@gmail.com>
2022-09-02 10:30:14 +0530

    errors due to redux

--- client/src/App.js
+++ client/src/App.js
@@ -6,20 +6,25 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
 const App = () => (
-  <BrowserRouter>
-    <Fragment>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Landing />} />
-      </Routes>
-      <section className='container'>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Fragment>
+        <Navbar />
         <Routes>
-          <Route path='/register' element={<Register />} />
-          <Route path='/login' element={<Login />} />
+          <Route path='/' element={<Landing />} />
         </Routes>
-      </section>
-    </Fragment>
-  </BrowserRouter>
+        <section className='container'>
+          <Routes>
+            <Route path='/register' element={<Register />} />
+            <Route path='/login' element={<Login />} />
+          </Routes>
+        </section>
+      </Fragment>
+    </BrowserRouter>
+  </Provider>
 );
 export default App;
